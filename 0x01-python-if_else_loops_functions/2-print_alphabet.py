8586e104036df23a43431441fae6965686f1cf7e
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-03 11:32:41 -0700

    if

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for alph in range(25, -1, -1):
     character = alph + ord('A')
-    if alph % 2 ==1:
+    if alph % 2 == 1:
         character += 32
-    print("{:c}".format(character), end=""
+    print("{:c}".format(character), end="")
