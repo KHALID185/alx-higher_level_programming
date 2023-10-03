c2861b19d918ba6cf3198378966e378a983aa0cf
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-03 10:17:06 -0700

    if

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for num in range(0, 10):
     for num2 in range(num, 10):
         if num < num2:
-            if num != 8 and num2 != 9:
+            if num != 8 or num2 != 9:
                 print("{:d}{:d}".format(num, num2), end=", ")
             else:
                 print("{:d}{:d}".format(num, num2), end="\n")
