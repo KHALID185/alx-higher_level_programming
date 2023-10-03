f57ebdb7bfb7723b5dd46cb9da0bf99aae040bac
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-03 09:51:28 -0700

    if

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabet in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(alphabet), end="")
+for alph in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(alph), end="")
