0d2c8235aadfe9e9faebb6d02d02af932ebc1b36
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-03 11:30:56 -0700

    if

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for alph in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(alph), end="")
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for alph in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(alph), end="")
+for alph in range(25, -1, -1):
+    character = alph + ord('A')
+    if alph % 2 ==1:
+        character += 32
+    print("{:c}".format(character), end=""
