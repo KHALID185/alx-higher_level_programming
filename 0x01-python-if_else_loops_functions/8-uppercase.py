d8b163575f7837f6e5e437db5cf8eb14af4e3e9c
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-03 10:28:32 -0700

    if

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
+def uppercase(str):
+    for c in str:
+        if islower(c)
+            print("{:c}".format(ord(c) - 32), end="")
+        else:
+            print("{:c}".format(ord(c), end="")
