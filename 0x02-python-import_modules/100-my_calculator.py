5a246ffcb91851b881fcabdd3b181f3c79ed3102
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-05 14:34:10 -0700

    adv_import

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a, b , o = int(sys.argv[1]), int(sys.argv[3]), sys.argv[2]
+    a, b, o = int(sys.argv[1]), int(sys.argv[3]), sys.argv[2]
 
     if o == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
