d7b9355d2dd732c38398372aee96149d75c75d3a
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-05 10:42:13 -0700

    imprt

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,9 @@
 if __name__ == "__main__":
 
     import sys
+
     addition = 0
-    for number in range (len(sys.argv) - 1):
+
+    for number in range(len(sys.argv) - 1):
         addition += int(sys.argv[number + 1])
     print(addition)
