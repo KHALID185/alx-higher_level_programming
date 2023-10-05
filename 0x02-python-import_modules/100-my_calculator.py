6ec44521598b5e855ad7b45f96e7849f691720c0
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-05 14:26:32 -0700

    adv_import

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,10 +6,14 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a, b , o = int(sys.argv[1]), int(sys.argv[3]), sys.argv[2]
-    if o == "+": print("{} + {} = {}".format(a, b, add(a, b)))
-    elif o == "-": print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif o == "/": print("{} / {} = {}".format(a, b, div(a, b)))
-    elif o == "*": print("{} * {} = {}".format(a, b, mul(a, b)))
+    if o == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif o == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif o == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    elif o == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
