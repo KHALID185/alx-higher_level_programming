37facf5693f071991de7ce1052aee65d1a9b2556
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-05 10:14:10 -0700

    imprt

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator import add, sub, div, mul
+    a, b = 10, 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
