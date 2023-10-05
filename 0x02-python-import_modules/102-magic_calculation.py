0eb852bd64a2e8570bec3c92c2595b7de1f744d0
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-05 15:21:35 -0700

    adv_import

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,10 +4,10 @@ def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for num in range(4, 6):
-            c = add(c, num)
-        return c
+            res = add(c, num)
+        return res
     else:
-        num2 = sub(a, b)
-        return num2
+        num_2 = sub(a, b)
+        return num_2
     return 0
 
