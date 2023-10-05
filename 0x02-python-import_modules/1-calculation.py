f2f68d7bdc042f25279c5254c66652982aee883a
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-05 10:17:33 -0700

    imprt

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, div, mul
+    from calculator_1 import add, sub, mul, div
     a, b = 10, 5
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
