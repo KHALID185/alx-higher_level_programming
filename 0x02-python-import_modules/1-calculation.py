83f3ac45a67241d723ea4cefc57f1cccb14f5d63
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-06 08:49:46 -0700

    second_l

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,8 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
-    a, b = 10, 5
+    a = 10
+    b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
