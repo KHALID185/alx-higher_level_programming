3567fa272f5c1c3eb33f1184a4aaa03058fdbe01
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-09 07:13:27 -0700

    lists

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    after_rem =""
+    for st in range(len(my_string)):
+        if (my_string[st] != 'c' and my_string[st] != 'C'):
+            after_rem += my_string[st]
+    return after_rem
