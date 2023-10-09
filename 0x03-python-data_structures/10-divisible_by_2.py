4328ac935b641d4732aa5f75d2fb4e9bd161bc4c
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-09 08:46:43 -0700

    lists

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+    lst = []
+    for items in my_list:
+        if(items % 2) == 0:
+            lst.append(True)
+        else:
+            lst.append(False)
+    return lst
