0f41413e9d85882d3d8ca4f6721bb7e95b65fbd5
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-09 08:38:54 -0700

    lists

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) < 1:
+        return None
+    else:
+        lst = my_list.copy()
+        lst.sort()
+        return (len(lst) - 1)
