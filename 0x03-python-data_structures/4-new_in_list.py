38586799c6ca12b7baa0337b4f634422c65b29ce
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-09 07:02:09 -0700

    lists

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-cpy = [None] * len(my_list)
-dx = 0
-l = len(my_list) - 1
-if idx < 0 or idx > l:
-    return my_list
-while dx <= l:
-    if dx == idx:
-        cpy[dx] = element
-    else:
-        cpy[dx] = my_list[dx]
-    dx += 1
-return cpy
+    cpy = [None] * len(my_list)
+    dx = 0
+    l = len(my_list) - 1
+    if idx < 0 or idx > l:
+        return my_list
+    while dx <= l:
+        if dx == idx:
+            cpy[dx] = element
+        else:
+            cpy[dx] = my_list[dx]
+        dx += 1
+    return cpy
