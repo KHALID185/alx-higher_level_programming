a6abffd7be236afa03de1bf1161818a72bc35508
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-11 10:05:12 -0700

    0x04

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,8 +3,7 @@ def weight_average(my_list=[]):
     if my_list is None:
         return 0
     else:
-        for x,y in my_list:
+        for x, y in my_list:
             s1 += x * y
             s2 += y
-        s = s1 / s2
-        return s
+        return s1 / s2
