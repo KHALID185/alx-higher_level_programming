b325bab78c4951fa3da5f1efa316ac7166ae7d6c
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-11 09:23:18 -0700

    0x04

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
+    d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M", 1000}
+    n_r = 0
+    c = 0
+    for items in reversed(roman_string):
+        n_r = d[items]
+        if c < n_r * 5:
+            c += n_r
+        else:
+            c = -n_r
+        return c
