cfa85f8a72b6da81d8c49ad6735d3a746323d5f8
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-11 10:14:53 -0700

    0x04

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    resultat = list(map(lambda i: list(map(lambda j: j**2, i)), matrix))
-    return resultat
+    return list(map(lambda i: list(map(lambda j: j**2, i)), matrix))
