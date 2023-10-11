5950bc2438afb84e7e9bf95a4135e68b29dfa7e4
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-10 20:45:52 -0700

    dic set

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    res = map(lambda num: list(map(lambda items: items ** 2, num), matrix)
+    squared = list(res)
+    return squared
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+sets, dict
