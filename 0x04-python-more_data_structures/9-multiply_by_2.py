28fa0012e762655d8f7716107d4fa3b0db7cef7c
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-11 08:29:35 -0700

    0x04

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return (ky: val * 2 for ky, val in a_dictionary.items())
+    d = dict()
+    for ky, val in a_dictionary.items())
+        d[ky] = val * 2
+    return d
