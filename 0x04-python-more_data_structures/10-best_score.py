c1148b95760ba4048db77112b02dd6f5a4375336
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-11 09:04:30 -0700

    0x04

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
+    if not a_dictionary:
+        return (None)
     vm = 0
     for val in a_dictionary.values():
         if val > vm:
