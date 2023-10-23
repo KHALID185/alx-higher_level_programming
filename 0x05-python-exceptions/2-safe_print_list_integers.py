857e5bc5902cf8cb7b8d45b2ff16c28111a73e1c
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-23 04:55:48 -0700

    exception

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    k = 0
+    while j < x:
+        try:
+            print("{:d}".format(my_list[j]), end="")
+            k += 1
+        except (ValueError, TypeError):
+            pass
+        j += 1
+    print()
+    return k
