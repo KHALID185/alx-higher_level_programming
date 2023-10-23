249f2b20c1efd45ca84684fc257bcc2e288d7121
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-23 06:18:51 -0700

    exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         garage = fct(*args)
         return garage
-    expect Exception as exp:
+    except Exception as exp:
         print("Exception: {}".format(exp), file=sys.stderr)
         return None
