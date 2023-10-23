0b6801f5086ce9b67e806bce9ecec4a207fd4043
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2023-10-23 18:09:57 +0100

    exception

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -2,14 +2,14 @@
 #include <Python.h>
 
 /**
- * print_python_bytes - Prints bytes information
- * @p: Python Object
- * Return: no return
+ * print_python_bytes - bytes prints
+ * @p: pointer
+ * Return: void
  */
 void print_python_bytes(PyObject *p)
 {
-	char *string;
-	long int size, i, limit;
+	char *Stt;
+	long int iterr, l_value, szz;
 
 	setbuf(stdout, NULL);
 
@@ -21,39 +21,38 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	size = ((PyVarObject *)(p))->ob_size;
-	string = ((PyBytesObject *)p)->ob_sval;
+	szz = ((PyVarObject *)(p))->ob_size;
+	Stt = ((PyBytesObject *)p)->ob_sval;
 
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", string);
+	printf("  size: %ld\n", szz);
+	printf("  trying string: %s\n", Stt);
 
-	if (size >= 10)
-		limit = 10;
+	if (szz >= 10)
+		l_value = 10;
 	else
-		limit = size + 1;
+		l_value = szz + 1;
 
-	printf("  first %ld bytes:", limit);
+	printf("  first %ld bytes:", l_value);
 
-	for (i = 0; i < limit; i++)
-		if (string[i] >= 0)
-			printf(" %02x", string[i]);
+	for (iterr = 0; iterr < l_value; iterr++)
+		if (Stt[iterr] >= 0)
+			printf(" %02x", Stt[iterr]);
 		else
-			printf(" %02x", 256 + string[i]);
+			printf(" %02x", 256 + Stt[iterr]);
 
 	printf("\n");
 	setbuf(stdout, NULL);
 }
 
 /**
- * print_python_float - Prints float information
- *
- * @p: Python Object
- * Return: no return
+ * print_python_float - print float numbers
+ * @p: pointer
+ * Return: Empty
  */
 void print_python_float(PyObject *p)
 {
-	double val;
-	char *nf;
+	char *strg;
+    double flt_num;
 
 	setbuf(stdout, NULL);
 	printf("[.] float object info\n");
@@ -65,24 +64,25 @@ void print_python_float(PyObject *p)
 		return;
 	}
 
-	val = ((PyFloatObject *)(p))->ob_fval;
-	nf = PyOS_double_to_string(val, 'r', 0, Py_DTSF_ADD_DOT_0, Py_DTST_FINITE);
+	flt_num = ((PyFloatObject *)(p))->ob_fval;
+	strg = PyOS_double_to_string(flt_num, 'r', 0, Py_DTSF_ADD_DOT_0, Py_DTST_FINITE);
 
-	printf("  value: %s\n", nf);
+	printf("  value: %s\n", strg);
 	setbuf(stdout, NULL);
 }
 
 /**
- * print_python_list - Prints list information
- *
- * @p: Python Object
- * Return: no return
+ * print_python_list - lists prints
+ * @p: pointer
+ * Return: Void
  */
+
 void print_python_list(PyObject *p)
 {
-	long int size, i;
-	PyListObject *list;
-	PyObject *obj;
+	long int lmt_sz;
+    long int itrr;
+	PyListObject *lsst;
+	PyObject *o_b_j;
 
 	setbuf(stdout, NULL);
 	printf("[*] Python list info\n");
@@ -94,21 +94,21 @@ void print_python_list(PyObject *p)
 		return;
 	}
 
-	size = ((PyVarObject *)(p))->ob_size;
-	list = (PyListObject *)p;
+	lmt_sz = ((PyVarObject *)(p))->ob_size;
+	lsst = (PyListObject *)p;
 
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
+	printf("[*] Size of the Python List = %ld\n", lmt_sz);
+	printf("[*] Allocated = %ld\n", lsst->allocated);
 
-	for (i = 0; i < size; i++)
+	for (itrr = 0; itrr < lmt_sz; itrr++)
 	{
-		obj = list->ob_item[i];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		o_b_j = lsst->ob_item[itrr];
+		printf("Element %ld: %s\n", itrr, ((o_b_j)->ob_type)->tp_name);
 
-		if (PyBytes_Check(obj))
-			print_python_bytes(obj);
-		if (PyFloat_Check(obj))
-			print_python_float(obj);
+		if (PyBytes_Check(o_b_j))
+			print_python_bytes(o_b_j);
+		if (PyFloat_Check(o_b_j))
+			print_python_float(o_b_j);
 	}
 	setbuf(stdout, NULL);
 }
