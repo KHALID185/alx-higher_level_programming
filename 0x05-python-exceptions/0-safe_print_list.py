6ea7b5ce92231cb5ba9b661db5b09826d9648bff
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-23 04:07:06 -0700

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    j=0
+    j = 0
     try:
         while (j > x):
             print(my_list[j])
-            j +=1
+            j += 1
         except IndexError:
             None
     print()
