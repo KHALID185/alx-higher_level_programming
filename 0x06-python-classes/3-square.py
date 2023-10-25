cf338bbbbcabc35eefcb2b1ad1a91059fa7d96a8
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-25 05:32:21 -0700

    task0

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,6 +14,8 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     def area(self):
+
         """public instance"""
         return self.__size ** 2
