4212ff921afe9578175d5402029796c13e159bca
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-25 05:42:34 -0700

    task0

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""class with instance prvt, public and raise the error"""
+
+
+class Square:
+    """initialize private attribute"""
+
+    def __init__(self, size=0):
+
+        """add size as argument"""
+        self.__size = size
+
+    @property
+    def size(self):
+
+        """property to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        """property setter"""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+
+        """public instance"""
+        return self.__size ** 2
