8ea282e1c4c53076948bc91704e5c4bbef0797a5
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-25 08:16:55 -0700

    tasks_OOP

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""define a class magic"""
+
+import math
+
+
+class MagicClass:
+    """represente the class magic and all the fct"""
+
+    def __init__(self, radius=0):
+        """initialization of arguments"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """the area of the magic calculateur"""
+        return self.__radius ** 2 * math.pi
+    def circumference(self):
+        """the circumference of the calcl"""
+        return 2 * math.pi * self.__radius
