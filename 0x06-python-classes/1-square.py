dbe745398348787367f1b70ef8f8134977cc5767
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-25 05:20:00 -0700

    task0

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""class with instance privite size"""
 
 
 class Square:
+    """initialize private attribute"""
+
     def __init__(self, size):
+
+        """add size as argument"""
         self.__size = size
