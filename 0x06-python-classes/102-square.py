d269126c84304d6fec7611eb2dd0f4206188779c
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-25 08:08:33 -0700

    tasks_OOP

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -31,19 +31,20 @@ class Square:
         """public instance"""
         return self.__size ** 2
 
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
     def __gt__(self, other):
         return self.area() > other.area()
 
     def __ge__(self, other):
         return self.area() >= other.area()
 
-    def __le__(self, other):
-        return self.area() <= other.area()
-
-    def __eq__(self, other):
-        return self.area() == other.area()
-
     def __lt__(self, other):
         return self.area() < other.area()
-    def __ne__(self, other):
-        return self.area() != other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
