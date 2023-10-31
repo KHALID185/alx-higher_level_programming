10323a543beff9b23ff1bfd94b659e79a1cadf2c
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-30 17:40:42 -0700

    more OOP

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -75,10 +75,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """the bigger of two numbers
-        args: rectangle 1, rectangle 2
-        
-        """
+        """the bigger of two areas of rectangle"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
