70e7fb8e7d271b755c52ac18c095f95e57f73e18
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2023-10-31 02:38:30 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,57 +1,62 @@
 #!/usr/bin/python3
-"""solves N queen problem """
+"""
+nqueens backtracking program to print the coordinates of n queens
+on an nxn grid such that they are all in non-attacking positions
+"""
 
 
 from sys import argv
 
 if __name__ == "__main__":
-    l_n_q = []
+    a = []
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
     if argv[1].isdigit() is False:
         print("N must be a number")
         exit(1)
-    nrd = int(argv[1])
-    if nrd < 4:
+    n = int(argv[1])
+    if n < 4:
         print("N must be at least 4")
         exit(1)
 
-    for items in range(nrd):
-        l_n_q.append([items, None])
+    # initialize the answer list
+    for i in range(n):
+        a.append([i, None])
 
-    def nrd_deja_existé(coord_y):
-        """check if the nord already exist or not"""
-        for items in range(nrd):
-            if coord_y == l_n_q[items][1]:
+    def already_exists(y):
+        """check that a queen does not already exist in that y value"""
+        for x in range(n):
+            if y == a[x][1]:
                 return True
         return False
 
-    def ref_sol(coord_x, coord_y):
-        """reject the solution if it is already exist"""
-        if (nrd_deja_existé(coord_y)):
+    def reject(x, y):
+        """determines whether or not to reject the solution"""
+        if (already_exists(y)):
             return False
-        items = 0
-        while(items < coord_x):
-            if abs(l_n_q[items][1] - coord_y) == abs(items - coord_x):
+        i = 0
+        while(i < x):
+            if abs(a[i][1] - y) == abs(i - x):
                 return False
-            items += 1
+            i += 1
         return True
 
-    def all_reset(coord_x):
-        """if the failaire exist clear all"""
-        for items_1 in range(coord_x, nrd):
-            l_n_q[items_1][1] = None
+    def clear_a(x):
+        """clears the answers from the point of failure on"""
+        for i in range(x, n):
+            a[i][1] = None
 
-    def nqueens(coord_x):
-        """find the solution with backtraking method"""
-        for items_y in range(nrd):
-            all_reset(coord_x)
-            if ref_sol(coord_x, items_y):
-                l_n_q[coord_x][1] = items_y
-                if (coord_x == nrd - 1):
-                    print(l_n_q)
+    def nqueens(x):
+        """recursive backtracking function to find the solution"""
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                a[x][1] = y
+                if (x == n - 1):  # accepts the solution
+                    print(a)
                 else:
-                    nqueens(coord_x + 1)
-# debut de recursive is 0
+                    nqueens(x + 1)  # moves on to next x value to continue
+
+    # start the recursive process at x = 0
     nqueens(0)
