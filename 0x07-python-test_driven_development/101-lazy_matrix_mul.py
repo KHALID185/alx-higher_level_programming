0d384abfdeb7d27d76a8be1f8291705b332102d9
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-31 16:34:03 -0700

    TDD

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""numpy module"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    maltiplucation of matrix a and b
+    Args:
+    2 matrix a and b
+    returns:
+    the resultats
+    """
+    return numpy.matmul(m_a, m_b)
