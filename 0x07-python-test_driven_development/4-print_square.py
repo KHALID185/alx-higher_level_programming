cc2577887d0cd91dab65de56ab91d873ccc9a6ef
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-01 07:03:10 -0700

    TDD

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -20,12 +20,13 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
     for line in range(size):
         for col in range(size):
             print("#", end="")
         if line != size - 1:
             print()
+    if size != 0:
+        print()
 
 
 if __name__ == "__main__":
