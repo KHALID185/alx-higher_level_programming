db0fea36e0f6d59b9276fa0ff77c54ee39412724
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-06 07:48:04 -0800

    inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module class"""
+
+
+class BaseGeometry:
+    """basegeometry class
+    method area()
+    method integer_validator(self, name, value)
+    """
+    def area(self):
+        """method area"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """method two integer validiton
+        args: name should be string"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
