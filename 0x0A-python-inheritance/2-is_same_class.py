bfc14de682d552649263745cde799baf389a9c50
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-06 05:41:38 -0800

    inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""fct verify if object is an instance of the specified class"""
+"""fct verify if object is an instance of class inherited from"""
 
 
 def is_same_class(obj, a_class):
     """
-    function return true if object is in the class
+    function return true if object is in the class inherited from
 
     args: the object and specified class
 
     return: true or false
     """
-    return type(obj) == a_class
+    return isinstance(obj, a_class)
