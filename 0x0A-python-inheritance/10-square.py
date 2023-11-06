b2adcdd658f0126199cb4f1fe5f89f4cddbe175c
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-06 08:27:37 -0800

    inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """"intialize a subclass of derived class"""
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """area method"""
+        return self.__size * self.__size
