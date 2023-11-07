9284d29d1f8032967e176f11a29181a5f86f88be
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-07 05:43:25 -0800

    I/O

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """read filename"""
-    with open(filename, encode='utf-8') as file:
+    with open(filename, encoding='utf-8') as file:
         rd = file.read()
     print(rd, end="")
