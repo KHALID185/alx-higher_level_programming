5e0e857b08c99a985e8f7252d25b875afdd97fcc
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-07 08:09:11 -0800

    In/Out

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""fct insert a line of a text in file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append a line after a search string"""
+    with open(filename, 'r', encoding="utf-8") as file:
+        line = []
+        while 1:
+            ln = file.readline()
+            if ln == "":
+                break
+            line.append(ln)
+            if search_string in ln:
+                line.append(new_string)
+    with open (filename, 'w', encoding="utf-8") as file:
+        f.writelines(line)
