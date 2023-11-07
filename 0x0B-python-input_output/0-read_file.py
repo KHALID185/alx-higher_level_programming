83367062776ef91380b0e85b8cfbc9a948961cac
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-07 05:42:04 -0800

    I/O

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that reads a text file"""
+
+
+def read_file(filename=""):
+    with open(filename, encode='utf-8') as file:
+        rd = file.read()
+    print(rd, end="")
