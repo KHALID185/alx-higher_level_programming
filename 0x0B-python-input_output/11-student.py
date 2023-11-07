ed6b9647ba85611d9f992a5e91eefb6d027f93fd
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-07 06:58:11 -0800

    I/O

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -27,7 +27,7 @@ class Student:
         return dct
 
     def reload_from_json(self, json):
-        """eplaces all attributes of the Student instance"""
+        """replaces all attributes of the Student instance"""
         for ky, val in json.items():
-            if ky in self.__dict:
+            if ky in self.__dict__:
                 self.__dict__[key] = val
