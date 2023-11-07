753b2d0b9321dd06ad5b103ef7ba6e8900ae8f13
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-07 08:27:23 -0800

    In/Out

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""script read line by line in stdin"""
+
+
+from sys import stdin
+
+
+code_status = {
+        '200' : 0, '301' : 0, '400' : 0, '401' : 0, '403' : 0, '404' : 0, '405' : 0, '500' : 0
+        }
+size = 0
+
+def fct_print():
+    """prints statistics since the beginning"""
+    print(f"File size: {size}")
+    for ky, val in sorted(code_status.items()):
+        if val > 0:
+            print("{:s}: {:d}".format(ky, val))
+
+j = 0
+try:
+    for ln in stdin:
+        spline = line.split()
+        if len(spline) >= 2:
+            stts = spline[-2]
+            size += int(spline[-1])
+            if stts in code_status:
+                code_status[stts] += 1
+        j += 1
+
+        if j % 10 == 0:
+            fct_print()
+        fct_print()
+
+except KeyboardInterrupt as exp:
+    fct_print()
