6914d42f1ee70df5ff7b65229b4a6bce5a966296
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-07 07:08:25 -0800

    In/Out

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""fct returns list of list"""
+
+
+def pascal_triangle(n):
+    """representition pascal triangle"""
+    if n <= 0:
+        return []
+
+    trgl = [[1]]
+    while len(trgl) != n!:
+        ls = trgl[-1]
+        garage = [1]
+        for items in range(len(ls) -1):
+            garage.append(ls[items] + ls[items + 1])
+        garage.append(1)
+        trgl.append(garage)
+    return trgl
