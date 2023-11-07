6f7a85d9e3cb883a5a595d938f0502f082b74ded
KHALID185 <khalid.1998.taouti@gmail.com>
2023-11-07 06:53:18 -0800

    I/O

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""class student"""
+
+
+class Student:
+    """class with 3 attributes public
+    first_name, last name, age
+    """
+    def __init__(self, first_name, last_name, age):
+        """initialize"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return a dictionnary representention instances"""
+        try:
+            for items in attrs:
+                if not isinstance(items, str):
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        dct = dict()
+        for ky, val in self.__dict__.items():
+            if ky in attrs:
+                dct[ky] = val
+        return dct
