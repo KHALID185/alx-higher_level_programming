f4fc9fcee76143bb671822121ded3de2f387f50e
KHALID185 <khalid.1998.taouti@gmail.com>
2024-05-23 14:16:15 -0700

    quereis to sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,9 @@
+-- cmmd list all tv show by rating
+-- ordred desc
+SELECT tv_shows.title,
+	SUM(tv_show_ratings.rate) AS rating
+	FROM tv_shows
+	INNER JOIN tv_show_ratings
+	ON tv_show_ratings.show_id = tv_shows.id
+	GROUP BY tv_shows.title
+	ORDER BY rating DESC;
