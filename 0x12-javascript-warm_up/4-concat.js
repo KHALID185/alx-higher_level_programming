47889141762ce425236a0e8c9e534cd33599a62f
KHALID185 <khalid.1998.taouti@gmail.com>
2024-06-18 14:13:05 -0700

    js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const process = require('process');
-const arg_2 = process.argv[2];
-const arg_3 = process.argv[3];
-console.log(`${arg_2} is ${arg_3}`);
+const arg22 = process.argv[2];
+const arg33 = process.argv[3];
+console.log(`${arg22} is ${arg33}`);
