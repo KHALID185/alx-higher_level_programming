1741f473d94e7e5db338a86de15e71b7a3c1c9fd
KHALID185 <khalid.1998.taouti@gmail.com>
2024-06-24 08:33:02 -0700

    ORM python

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,10 @@ if __name__ == "__main__":
         db=sys.argv[3],
         charset="utf8")
     cur = conn.cursor()
-    userinput = sys.argv[4]
     cur.execute(
-        f"SELECT * FROM states WHERE name LIKE '%%{userinput}%%' ORDER BY id ASC")
+        "SELECT * FROM states WHERE name LIKE '%%{userinput}%%' \ 
+         ORDER BY id ASC".format(
+        userinput = sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
