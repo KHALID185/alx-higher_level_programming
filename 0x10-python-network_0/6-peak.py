2c959625f4d13d24a8cff7fd9c9eaa40034ae2c9
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2024-07-04 12:06:04 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-""" define Peak value in a list"""
+""" Finds a peak in a list of unsorted integers
+"""
 
 
 def find_peak(list_of_integers):
-    """Find the peak in a list of integer unsorted"""
-    list_i = len(list_of_integers)
-    if list_of_integers == []:
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
         return None
-    peak = bin_search(list_of_integers, 0, list_i - 1)
-    return list_of_integers[peak]
 
-
-""" binary search algorithim """
-
-
-def bin_search(aa, l, h):
-    """binary search of the peak"""
-    if l >= h:
-        return l
-    md = ((h - l) // 2) + l
-    if aa[md] > aa[md + 1]:
-        return bin_search(aa, l, md)
-    else:
-        return bin_search(aa, md + 1, h)
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
