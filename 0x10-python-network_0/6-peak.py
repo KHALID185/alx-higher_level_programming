55120e74d91fad8262f726e9dc6b89adf53c5f06
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2024-07-04 11:39:35 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""define a peak algorithm"""
+"""dunction to Defines a peak"""
 
 
 def find_peak(list_of_integers):
-    """find a peak in a list of integers"""
-    if list_of_integers:
-        pk = 0
-        xl = len(list_of_integers) - 1
-        while pk < x:
-            m = (p+x) // 2
-            if list_of_integers[m] > list_of_integers[m + 1]:
-                xl = m
-            else:
-                pk = m + 1
-        return list_of_integers[pk]
+    """peak in a list of int"""
+    if list_of_integers == []:
+        return None
+
+    sz = len(list_of_integers)
+    if sz == 1:
+        return list_of_integers[0]
+    elif sz == 2:
+        return max(list_of_integers)
+
+    md = int(sz / 2)
+    peak = list_of_integers[md]
+    if peak > list_of_integers[md - 1] and peak > list_of_integers[md + 1]:
+        return peak
+    elif peak < list_of_integers[md - 1]:
+        return find_peak(list_of_integers[:md])
+    else:
+        return find_peak(list_of_integers[md + 1:])
