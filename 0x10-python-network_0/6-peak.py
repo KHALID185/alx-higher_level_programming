b963263ed14fd243e08e407adeeaef12cafbe805
KHALID185 <khalid.1998.taouti@gmail.com>
2024-07-04 04:41:15 -0700

    python network 0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,8 +4,14 @@
 
 def find_peak(list_of_integers):
     """Find a peak in a list of unsorted integers."""
-    if not list_of_integers:
+    if list_of_integers == []:
         return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
     return list_of_integers[bin_search(list_of_integers, 0, len(list_of_integers) - 1)]
 
 
