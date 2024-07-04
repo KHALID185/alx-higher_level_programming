86412f3b4d477f27b476466c8e29ec9092de4ee8
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2024-07-04 12:35:56 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-""" define Peak value in a list"""
+"""Define a function to find a peak in a list of unsorted integers."""
 
 
 def find_peak(list_of_integers):
-    """Find the peak in a list of integer unsorted"""
-    list_i = len(list_of_integers)
-    if list_of_integers == []:
+    """Find a peak in a list of unsorted integers."""
+    if not list_of_integers:
         return None
-    peak = bin_search(list_of_integers, 0, list_i - 1)
-    return list_of_integers[peak]
+    return list_of_integers[bin_search(list_of_integers, 0, len(list_of_integers) - 1)]
 
 
-""" binary search algorithim """
-
-
-def bin_search(aa, l, h):
-    """binary search of the peak"""
-    if l >= h:
-        return l
-    md = ((h - l) // 2) + l
-    if aa[md] > aa[md + 1]:
-        return bin_search(aa, l, md)
-    else:
-        return bin_search(aa, md + 1, h)
+def bin_search(arr, low, high):
+    """Binary search algorithm to find a peak."""
+    if low == high:
+        return low
+    mid = (low + high) // 2
+    if arr[mid] > arr[mid + 1]:
+        return bin_search(arr, low, mid)
+    return bin_search(arr, mid + 1, high)
