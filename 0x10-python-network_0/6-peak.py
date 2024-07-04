3315a8c7a6bbffa150add411b6657d87d1872430
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2024-07-04 12:21:18 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,8 +3,6 @@
 
 
 def find_peak(list_of_integers):
-    """peak in a list of int"""
-    if list_of_integers == []:
     """Find the peak in a list of integer unsorted"""
     list_i = len(list_of_integers)
     if list_i is 0:
@@ -12,18 +10,7 @@ def find_peak(list_of_integers):
     peak = bin_search(list_of_integers, 0, list_i - 1)
     return list_of_integers[peak]
 
-    sz = len(list_of_integers)
-    if sz == 1:
-        return list_of_integers[0]
-    elif sz == 2:
-        return max(list_of_integers)
 
-    md = int(sz / 2)
-    peak = list_of_integers[md]
-    if peak > list_of_integers[md - 1] and peak > list_of_integers[md + 1]:
-        return peak
-    elif peak < list_of_integers[md - 1]:
-        return find_peak(list_of_integers[:md])
 """ binary search algorithim """
 
 
@@ -35,5 +22,4 @@ def bin_search(aa, l, h):
     if aa[md] > aa[md + 1]:
         return bin_search(aa, l, md)
     else:
-        return find_peak(list_of_integers[md + 1:])
         return bin_search(aa, md + 1, h)
