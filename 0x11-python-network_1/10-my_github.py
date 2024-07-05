d753e5d7cdd4f65bdd029ae3c22ea113f1f02a5e
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2024-07-05 14:36:00 +0100

    Update 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that take a GitHub credentials and uses the GitHub api
+"""
+
+from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
