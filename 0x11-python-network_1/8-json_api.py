7c655446cfba297a48cb39ba14cba71d60af4fe1
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2024-07-05 14:33:00 +0100

    Update 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        letter = ""
+    else:
+        letter = argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': letter}
+    r = requests.post(url, data=payload)
+
+    try:
+        dic = r.json()
+        if dic:
+            print("[{}] {}".format(dic.get('id'), dic.get('name')))
+        else:
+            print("No result")
+    except ValueError as e:
+        print("Not a valid JSON")
