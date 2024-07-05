b6e01e9e659982625e59674c8075b096d146b5c6
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2024-07-05 14:31:49 +0100

    Update 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that sends a request to the URL
+and displays the body of the response
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
