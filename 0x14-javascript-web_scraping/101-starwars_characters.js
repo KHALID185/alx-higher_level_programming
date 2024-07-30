beaf0a8fa0fcb64356fb9e6f051b5233b8ae87a4
KHALID185 <58513767+KHALID185@users.noreply.github.com>
2024-07-30 14:56:10 +0100

    Update 101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
